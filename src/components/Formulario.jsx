1dcbfff4603db8048821d4906269d9abc6dac334
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2023-05-17 23:08:18 -0600

    Update Formulario.jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -22,7 +22,7 @@ function Formulario() {
             setNombre(paciente.nombre)
             setPropietario(paciente.propietario)
             setEmail(paciente.email)
-            setFecha(new Date(paciente.fecha).toLocaleDateString('es-MX'))
+            setFecha(new Date(paciente.fecha).toLocaleDateString('en-US'))
             setSintomas(paciente.sintomas)
             setId(paciente._id)
         }
